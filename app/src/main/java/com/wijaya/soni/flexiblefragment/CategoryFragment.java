e7e93026cdd3ec0124c7f8c51c1445bb5866a10e
Soni Wijaya <soni@MMDCs-MacBook-Air.local>
2018-08-22 16:19:08 +0700

    move fragment to another fragment with new data in one activity

---
+++ app/src/main/java/com/wijaya/soni/flexiblefragment/CategoryFragment.java
@@ -0,0 +1,39 @@
+package com.wijaya.soni.flexiblefragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CategoryFragment extends Fragment implements View.OnClickListener{
+    private Button btnDetailCategory;
+
+    public CategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        btnDetailCategory = (Button)view.findViewById(R.id.btn_detail_category);
+        btnDetailCategory.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_detail_category){
+
+        }
+    }
+}
--- app/src/main/java/com/wijaya/soni/flexiblefragment/HomeFragment.java
+++ app/src/main/java/com/wijaya/soni/flexiblefragment/HomeFragment.java
@@ -3,6 +3,8 @@ package com.wijaya.soni.flexiblefragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,7 +35,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.btn_category){
-
+            CategoryFragment mCategoryFragment = new CategoryFragment();
+            FragmentManager mFragmentManager = getFragmentManager();
+            FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+            mFragmentTransaction.replace(R.id.frame_container, mCategoryFragment, CategoryFragment.class.getSimpleName());
+            mFragmentTransaction.addToBackStack(null);
+            mFragmentTransaction.commit();
         }
     }
 }

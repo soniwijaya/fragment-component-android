2ae11355b984cd7a6e4c8d70939a238e8a4c3fcb
Soni Wijaya <soni@MMDCs-MacBook-Air.local>
2018-08-22 16:42:44 +0700

    jump to double fragment with bring data

--- app/src/main/java/com/wijaya/soni/flexiblefragment/CategoryFragment.java
+++ app/src/main/java/com/wijaya/soni/flexiblefragment/CategoryFragment.java
@@ -3,6 +3,8 @@ package com.wijaya.soni.flexiblefragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,7 +35,17 @@ public class CategoryFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.btn_detail_category){
-
+            DetailCategoryFragment mDetailCategoryFragment = new DetailCategoryFragment();
+            Bundle mBundle = new Bundle();
+            mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "LifeStyle");
+            String description = "Kategori ini akan berisi produk lifestyle";
+            mDetailCategoryFragment.setArguments(mBundle);
+            mDetailCategoryFragment.setDescription(description);
+            FragmentManager mFragmentManager = getFragmentManager();
+            FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+            mFragmentTransaction.replace(R.id.frame_container, mDetailCategoryFragment, DetailCategoryFragment.class.getSimpleName());
+            mFragmentTransaction.addToBackStack(null);
+            mFragmentTransaction.commit();
         }
     }
 }
